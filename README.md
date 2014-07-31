d6a8a271b0874a96cbd87a626d3503d02e2348d3
byndcivilization <byndcivilization@gmail.com>
2014-07-31 16:41:41 -0400

    test routes in  server.js

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ API_TEST
 ========
 
 Test deployment of API for MongoDB
+
+npm rebuild from root
+
+
+Dependancies
+------------
+*	[Express](https://www.npmjs.org/package/express "Express") - web framework
+*	[Mongoose](https://www.npmjs.org/package/mongoose "Mongoose") - MongoDB connector
+*	[BodyParser](https://www.npmjs.org/package/body-parser "BodyParser") - parsing middleware
--- server.js
+++ server.js
@@ -4,13 +4,18 @@
 // =============================================================================
 
 // call the packages we need
-var express    = require('express'); 		// call express
-var app        = express(); 				// define our app using express
-var bodyParser = require('body-parser');
+var express		= require('express'); 		// call express
+var app			= express(); 				// define our app using express
+var bodyParser  = require('body-parser');
+var mongoose	= require('mongoose');		// Create connetion to mongodb
+mongoose.connect('mongodb://node:node@localhost:27017');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser());
+
+// import models
+var Country		= require('./app/models/country')
 
 var port = process.env.PORT || 8080; 		// set our port
 
@@ -18,10 +23,32 @@ var port = process.env.PORT || 8080; 		// set our port
 // =============================================================================
 var router = express.Router(); 				// get an instance of the express Router
 
+// middleware to use for all requests
+router.use(function(req, res, next) {
+	// do logging
+	console.log('Something is happening.');
+	next(); // make sure we go to the next routes and don't stop here
+});
+
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
 	res.json({ message: 'hooray! welcome to our api!' });	
 });
+
+// define routes for countries collection
+router.route('/countries')
+	// get all the bears (accessed at GET http://localhost:8080/api/bears)
+	.get(function(req, res) {
+
+		Country.find(function(err, countries) {
+			if (err)
+				res.send(err);
+
+			res.json(countries);
+		});
+	});
+
 
 // more routes for our API will happen here
 
